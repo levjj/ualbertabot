18d7e938deb11ac338d8357d2bea9151be1d749a
Christopher Schuster <cs@livoris.de>
2014-11-12 11:05:38 -0800

    made bo-extractor walk through subdirectories

--- bo-extractor/extractor.py
+++ bo-extractor/extractor.py
@@ -1,11 +1,18 @@
 from pyreplib import replay
 import sys, os
 
+# Collects information about all players of a certain race
 class Race:
     def __init__(self, name):
         self.name = name;
         self.states = {}
+        # Create P/T/Z directory
+        if not os.path.exists(self.name[0]):
+            os.makedirs(self.name[0])
+        self.data = open(self.player.race_name[0] + '/data.csv', 'w')
 
+    # Returns code for the set of buildings, using auto-increment to
+    # create new codes for unseen building sets
     def getState(self, buildings):
         buildings.sort()
         buildings = str(buildings)
@@ -13,9 +20,13 @@ class Race:
             self.states[buildings] = len(self.states) + 1
         return self.states[buildings]
 
+    # Write one full game to data.csv
+    def write(self, lst):
+        self.data.write(','.join([str(i) for i in lst]))
+
+    # Write the mapping from codes to building sets to stats.csv
     def dump(self):
-        if not os.path.exists(self.name[0]):
-            os.makedirs(self.name[0])
+        self.data.close()
         stats = open(self.name[0] + '/stats.csv', 'w')
         state_desc = [0] * max([0] + self.states.values())
         for key, value in self.states.items():
@@ -31,6 +42,7 @@ races['Zerg'] = Race('Zerg')
 races['Terran'] = Race('Terran')
 races['Protoss'] = Race('Protoss')
 
+# Models information about a single game
 class Game:
     def __init__(self, fname):
         self.replay = replay.Replay(fname)
@@ -40,6 +52,7 @@ class Game:
         self.race = races[self.player.race_name]
         self.bucketSize = 300 # 1 frame = 42ms, 300 frames = 12.6s
 
+    # Model the opponent of the protoss player (ignores TvZ games, etc.)
     def parsePlayers(self):
         if len(self.replay.players) != 2:
             raise Exception("Expect 1on1 game")
@@ -49,11 +62,8 @@ class Game:
             self.player = self.replay.players[0] # Analyze other player
         else:
             raise Exception("No Protoss player involved")
-        
-    def buildAction(self, building_type_id):
-        if self.action_known():
-            pass
 
+    # Go through all actions in the game and put them in 12.6s buckets
     def bucketActions(self):
         self.buckets = {}
         for action in self.player.actions:
@@ -63,33 +73,43 @@ class Game:
                 bucket.append(action)
                 self.buckets[idx] = bucket
 
+    # Generate building set codes for every 12.6s of the game and write to file
     def analyze(self):
         self.bucketActions()
-
         #print [self.player.race_name, self.replay.map_name]
-        
-        if not os.path.exists(self.player.race_name[0]):
-            os.makedirs(self.player.race_name[0])
-        data = open(self.player.race_name[0] + '/data.csv', 'w')
-        
+
         buildings = []
+        trace = []
         for step in range(max(self.buckets.keys())+1):
             bucket = self.buckets.get(step, [])
             for action in bucket:
                 building = action.get_building_type()
                 if building not in buildings:
                     buildings.append(building)
-            data.write(str(self.race.getState(buildings)) + ',')
-        data.write('\r\n')
-        data.close()
+            trace.append(self.race.getState(buildings))
+        self.race.write(trace)
 
-# Expect name of replay file as first argument
-fname = sys.argv[1]
-try:
-    game = Game(fname)
-    game.analyze()
-    for race in races:
-        races[race].dump()
-    
-except Exception as e:
-    print fname, ": bad replay", str(e)
+    # If both players are Protoss, repeat analysis for other player, too
+    def analyze2(self):
+        if self.race.name == 'Protoss':
+            self.player = self.replay.players[0]
+            self.analyze()
+
+# Expect directory with replay files as first argument
+rdir = sys.argv[1]
+
+# Analyze all .rep files in directories below rdir
+for root, dirs, files in os.walk(rdir):
+    for f in files:
+        if f.lower().endswith(".rep"):
+            fname = os.path.join(root, f)
+            try:
+                game = Game(fname)
+                game.analyze()
+                print fname, ": success"
+            except Exception as e:
+                print fname, ": bad replay", str(e)
+
+# Write the accumulated building set mapping to stats.csv
+for race in races:
+    races[race].dump()
